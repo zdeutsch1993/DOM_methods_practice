8b7b8e9bdcac6d13dd92a50be6a25e395e4a005b
zdeutsch1993 <zdeutsch1993@gmail.com>
2020-06-16 22:19:43 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const newButton = document.createElement('button');
+document.body.appendChild(newButton)
+const newMain = document.createElement('main');
+document.body.appendChild(newMain)
+const newImg = document.createElement('img');
+newMain.appendChild(newImg)
+newImg.src = '182-1824450_house-stark-the-north-remembers.png';
+const newAlink = document.createElement('a');
+const link = document.createTextNode("a website");
+newAlink.appendChild(link);
+newAlink.title = "this is a website"
+newAlink.href = 'https://towardsdatascience.com/3-numpy-image-transformations-on-baby-yoda-c27c1409b411';
+newMain.appendChild(newAlink)
